c132c35b12648143da7fd6610ef0614094d2f2d8
Anton Stepanets <a.stepanets@revizto.com>
2022-03-24 11:21:21 +0100

    test

---
+++ ViewerShared/Assets/Scripts/TestHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class TestHelper 
+{
+    public static int Add(int a, int b) {
+        return a + b;
+    }
+
+    public static int Substract(int a, int b) {
+        return a - b;
+    }
+
+    public static int Max(int a, int b) {
+        return Mathf.Max(a, b);
+    }
+}
